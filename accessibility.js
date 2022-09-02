80b5ca05c13d702e966e538fccca97f28c7e474d
David Souther <davidsouther+github@gmail.com>
2022-09-01 20:49:20 -0700

    Jiffies CSS - Complete Browser Reset

---
+++ accessibility.js
@@ -0,0 +1,20 @@
+function tabChange(event) {
+  [
+    ...document.querySelectorAll(
+      `input[type="radio"][name="${event.target.name}"]`
+    ),
+  ].forEach(setTab);
+}
+
+function setTab(radio) {
+  let node = radio;
+  while (node.getAttribute("role") != "tab") node = node.parentNode;
+  node.setAttribute("aria-selected", radio.checked ?? false);
+}
+
+[...document.querySelectorAll('[role="tab"] input[type="radio"]')].forEach(
+  (tab) => {
+    tab.addEventListener("change", tabChange);
+    setTab(tab);
+  }
+);
