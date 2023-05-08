95ed2ecca7784e4442d01da17d498efa2fbdcd30
David Souther <davidsouther+github@gmail.com>
2023-05-08 09:40:44 -0700

    1.0.1 release

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-npx @parcel/css-cli --sourcemap --bundle index.css -o jiffies-css-bundle.css
-npx @parcel/css-cli --sourcemap --bundle --minify index.css -o jiffies-css-bundle.min.css
+npx @parcel/css-cli --sourcemap --bundle --nesting index.css -o jiffies-css-bundle.css
+npx @parcel/css-cli --sourcemap --bundle --nesting --minify index.css -o jiffies-css-bundle.min.css
