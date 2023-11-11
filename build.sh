cf3af99663c0e4b24aa6fc91bd8ff9561a1f4459
David Souther <davidsouther+github@gmail.com>
2023-11-11 08:49:50 -0500

    v2 bundle

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
-npx @parcel/css-cli --sourcemap --bundle --nesting index.css -o jiffies-css-bundle.css
-npx @parcel/css-cli --sourcemap --bundle --nesting --minify index.css -o jiffies-css-bundle.min.css
+# npx @parcel/css-cli --sourcemap --bundle --nesting index.css -o jiffies-css-bundle.css
+# npx @parcel/css-cli --sourcemap --bundle --nesting --minify index.css -o jiffies-css-bundle.min.css
+npx esbuild --sourcemap --bundle index.css --outfile=jiffies-css-bundle.css
+npx esbuild --sourcemap --bundle --minify index.css --outfile=jiffies-css-bundle.min.css
+npx esbuild --sourcemap --bundle v2/index.css --outfile=jiffies-css-v2-bundle.css
+npx esbuild --sourcemap --bundle --minify v2/index.css --outfile=jiffies-css-v2-bundle.min.css
